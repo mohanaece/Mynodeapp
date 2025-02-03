adb6401cf96796cf9f5a75278b607e6aaaa4f2d2
mohanaece <ec2-user@ip-172-31-10-122.ap-southeast-2.compute.internal>
2025-02-03 07:37:08 +0000

    changed app.js file

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World Mohana!\n');
+  res.end('Hello, World Mohana for git test!\n');
   res.end('Checking if local changes are pushed to git!\n');
 });
 
